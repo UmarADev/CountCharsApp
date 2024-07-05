e7add028bd48bbbb7a16ae55e8e5304916c58444
UmarADev <umargithub03@gmail.com>
2024-07-05 21:16:49 +0500

    Add project files.

---
+++ CountCharsApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using CountCharsApp.Services;
+using System;
+
+namespace CountCharsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isContinue;
+
+            do
+            {
+                Console.WriteLine("Hi! This app gives you count of numbers and letters count of your text.");
+                Console.WriteLine("Give me a text...");
+                string userInput = Console.ReadLine();
+
+                CountCharServices.GetCountValue(userInput);
+
+                Console.WriteLine("Do you want to try again? y / n");
+                isContinue = Console.ReadLine().ToLower();
+            } while (isContinue == "y");
+        }
+    }
+}
---
+++ CountCharsApp/Services/CountCharServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CountCharsApp.Services
+{
+    internal class CountCharServices
+    {
+        private static int numbersCount = 0;
+        private static int lettersCount = 0;
+        private static int otherCount = 0;
+
+        private static void GetCharacterCount(string userInput)
+        {
+            foreach (char character in userInput)
+            {
+                if (char.IsDigit(character))
+                {
+                    numbersCount++;
+                }
+                else if (char.IsLetter(character))
+                {
+                    lettersCount++;
+                }
+                else
+                {
+                    otherCount++;
+                }
+            }
+        }
+
+        public static void GetCountValue(string userInput)
+        {
+            GetCharacterCount(userInput);
+
+            Console.WriteLine($"LETTERS: {lettersCount}\n");
+            Console.WriteLine($"DIGITS: {numbersCount}\n");
+        }
+    }
+}
